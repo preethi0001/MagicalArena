9aea7359f50f6f646616078aa861e0135c4fe1be
GuruPreethi <preethi82003@gmail.com>
2024-05-04 12:28:13 +0530

    Player class implementation

---
+++ src/Player.java
@@ -0,0 +1,34 @@
+import java.util.Random;
+
+public class Player {
+    private int health;
+    private int strength;
+    private int attack;
+
+    public Player(int health, int strength, int attack) {
+        this.health = health;
+        this.strength = strength;
+        this.attack = attack;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public int rollDie() {
+        Random random = new Random();
+        return random.nextInt(6) + 1;
+    }
+}
